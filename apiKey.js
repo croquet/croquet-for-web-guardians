f663e8058e6b26f5aa20861f94def59381c66e1e
Vanessa Freudenberg <vanessa@codefrau.net>
2023-06-29 15:19:37 -0700

    comment for apiKey.js

--- apiKey.js
+++ apiKey.js
@@ -1,5 +1,14 @@
 const apiKey = ''; // <-- insert your API key from croquet.io/keys here
 
-export default {
-    apiKey,
-};
+export default { apiKey };
+
+// NOTE: Croquet API keys are "client-side" keys
+// and are safe to be checked into source control.
+//
+// They do NOT have to be kept secret. In your deployed web app,
+// the key will be visible in the source code anyways.
+// On the Croquet key site you can restrict a key to a specific domain.
+// If someone else tries to use your key on a different domain,
+// the Croquet server will reject the connection:
+//
+//     https://croquet.io/keys
