cfe2cecec5ae00797db90e4b7ef1f30701153cda
Vanessa Freudenberg <vanessa@codefrau.net>
2023-07-25 17:56:36 +0200

    show in-game QR code pointing to lobby with session

--- game.js
+++ game.js
@@ -13,14 +13,18 @@ const inIframe = window.parent !== window;
 const url = new URL(window.location.href);
 const sessionName = url.searchParams.get("session");
 url.pathname = url.pathname.replace(/[^/]*$/, "index.html");
-App.sessionURL = url.href; // for QR code
-if (!inIframe || !sessionName) window.location.href = App.sessionURL;
+if (!inIframe || !sessionName) window.location.href = url.href;
 
 // ensure unique session per lobby URL
 const BaseUrl = url.href.replace(/[^/?#]*([?#].*)?$/, "");
-Constants.LobbyUrl = BaseUrl + "index.html";    // hashed into sessionId
+Constants.LobbyUrl = BaseUrl + "index.html";    // hashed into sessionId without params
 
-App.makeWidgetDock();
+// QR code points to lobby, with session name in hash
+url.searchParams.delete("session");
+url.hash = encodeURIComponent(sessionName);
+App.sessionURL = url.href;
+
+App.makeWidgetDock({ iframe: true });
 StartWorldcore({
     ...apiKey,
     appId: 'io.croquet.guardians', // <-- feel free to change
